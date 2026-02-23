8df65be6da0b0e2221b4830bc9669b23f62991f3
Daniel Mello <danielmello0110@gmail.com>
2026-02-23 09:42:45 -0300

    atualizando url

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://goldfish-app-nk5x6.ondigitalocean.app/",
+  baseURL: "https://fedconnect-back-end-fedcorp-s3ho5.ondigitalocean.app/",
   // baseURL: "http://localhost:8000/",
 });
 
