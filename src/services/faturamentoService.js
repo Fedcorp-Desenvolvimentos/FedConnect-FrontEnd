afbd199f5c5019cb0303603cdc719a32d20bee62
Leonan Thomaz <leonan.thomaz@gmail.com>
2026-06-12 14:30:04 -0300

    Ajustando endpoint de consulta

---
+++ src/services/faturamentoService.js
@@ -0,0 +1,12 @@
+import api from "./api";
+
+const getDadosBoletosSegundaVia = async (fatura) => {
+  try {
+    const response = await api.get(`api/faturamento/dados-segunda-via/${fatura}/`);
+    console.log("Resposta da API:", response.data);
+    return response.data;
+    } catch (error) {
+        console.error("Erro ao obter dados dos boletos:", error);
+        throw error;
+  }
+};
