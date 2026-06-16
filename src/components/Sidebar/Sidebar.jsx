83c45ccbc4e846991d625524930cfc4a5695122d
Leonan <79343444+leonanthomaz@users.noreply.github.com>
2026-06-16 00:09:16 -0300

    Consertando bug da sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -82,7 +82,7 @@ function Sidebar({ sidebarOpen, setSidebarOpen, toggleSidebar }) {
     { path: "/automacao", label: "Automação", icon: <FaCog />, allowed: emailsPermitidosAutomacao.includes(emailUsuario) ? ["admin"] : [] },
     { path: "/analytics", label: "Estatísticas", icon: <FaChartLine />, allowed: ["admin"] },
     { path: "/questionarios", label: "Questionário", icon: <FaClipboardList  />, allowed: ["admin", "usuario", "comercial", "faturamento", "ti"] },
- main
+ 
     // { path: "/workflow", label: "Workflow", icon: <FaProjectDiagram />, allowed: ["admin", "usuario", "comercial", "faturamento", "ti"] },
     // { path: "/rh", label: "Recursos Humanos", icon: <FaUsers />, allowed: ["admin"] },
   ];
