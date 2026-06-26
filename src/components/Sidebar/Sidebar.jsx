199429d0eff5c342f885f525aebcc4c63c22f138
Leonan Thomaz <leonan.thomaz@gmail.com>
2026-06-26 13:20:30 -0300

    Testando outra abordagem em nivel de acesso - automação

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -81,7 +81,7 @@ function Sidebar({ sidebarOpen, setSidebarOpen, toggleSidebar }) {
     { path: "/faturamento", label: "Faturamento", icon: <FaWallet />, allowed: ["admin", "faturamento", "ti"] },
     { path: "/metricas", label: "Métricas", icon: <FaChartBar />, allowed: ["admin"] },
     { path: "/agenda", label: "Agenda", icon: <FaCalendarAlt />, allowed: ["admin", "usuario", "comercial", "faturamento", "ti"] },
-    { path: "/automacao", label: "Automação", icon: <FaCog />, allowed: emailsPermitidosAutomacao.includes(emailUsuario) ? ["admin"] : [] },
+    { path: "/automacao", label: "Automação", icon: <FaCog />, allowed: emailsPermitidosAutomacao.includes(emailUsuario) ? ["admin", "faturamento", "ti", "usuario", "comercial"] : ["admin"] },
     { path: "/analytics", label: "Estatísticas", icon: <FaChartLine />, allowed: ["admin"] },
     { path: "/questionarios", label: "Questionário", icon: <FaClipboardList />, allowed: ["admin", "usuario", "comercial", "faturamento", "ti"] },
     { path: "/financeiro/comissao", label: "Emissão Voucher", icon: <FaFileInvoiceDollar  />, allowed: ["admin","ti"] },
