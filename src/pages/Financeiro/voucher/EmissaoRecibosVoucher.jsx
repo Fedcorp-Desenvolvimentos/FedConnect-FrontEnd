892240d859216196366ade50c832b1babfee97f6
Leonan <79343444+leonanthomaz@users.noreply.github.com>
2026-07-03 12:17:30 -0300

    Corrigindo bug camelcase PreviewModalDetails - tentativa 3

--- src/pages/Financeiro/voucher/EmissaoRecibosVoucher.jsx
+++ src/pages/Financeiro/voucher/EmissaoRecibosVoucher.jsx
@@ -8,7 +8,7 @@ import { FilterForm } from './components/FilterForm';
 import { ComissoesPanel } from './components/ComissoesPanel';
 import { EmissaoPanel } from './components/EmissaoPanel';
 import { RetencoesPanel } from './components/RetencoesPanel';
-import { PreviewModalDetailsDetails } from './components/PreviewModalDetailsDetails';
+import { PreviewModalDetails } from './components/PreviewModalDetails';
 import {
   Container,
   Header,
