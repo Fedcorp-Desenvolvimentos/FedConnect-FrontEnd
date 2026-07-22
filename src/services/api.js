7830e0792cc55263e00aa8d99e3bb1e510b374a5
Leonan Thomaz <leonan.thomaz@gmail.com>
2026-07-22 17:39:30 -0300

    Corrigindo endpoint do backend

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,7 @@ import axios from "axios";
 const ambiente = "dev";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_REACT_APP_URL_BACKEND ? import.meta.env.VITE_REACT_APP_URL_BACKEND : "http://localhost:8000/",
+  baseURL: "https://fedconnect-backend-d6kgr.ondigitalocean.app/",
 });
 
 // Intercepta todas as requisições Axios
